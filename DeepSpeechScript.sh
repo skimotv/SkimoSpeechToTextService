4923aa2bc58fc99d2263afc4b10d90669f65aa9c
Advait Sankaramanchi <47619280+Yawsama@users.noreply.github.com>
2020-12-09 16:45:31 -0600

    Deleting old script for version 0.6.0

--- DeepSpeechScript.sh
+++
@@ -1,4 +0,0 @@
-asset="$1"
-output="$2"
-echo $asset >> $output
-deepspeech --model /usr/local/bin/deepspeech-0.6.0-models/output_graph.pb --lm /usr/local/bin/deepspeech-0.6.0-models/lm.binary --trie /usr/local/bin/deepspeech-0.6.0-models/trie --audio $asset > $output
